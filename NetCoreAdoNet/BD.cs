c6685dacc5845471ee237c0743455b7c16b851e0
Diogo Rodrigo Schimmelpfennig <diogo_ti@hotmail.com.br>
2020-02-26 21:35:38 -0400

    Projeto de Exemplo ADO.NET

---
+++ NetCoreAdoNet/BD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace NetCoreAdoNet
+{
+    class BD
+    { 
+        private readonly string StrConexao;
+
+        public BD(string strconexao)
+        {
+            this.StrConexao = strconexao;
+        }
+
+        public void RunCommand(byte[] dados)
+        {
+            using var conexao = new SqlConnection(StrConexao);
+            using var comando = new SqlCommand("INSERT INTO TBL (dados) VALUES (@dados)", conexao);
+
+            comando.Parameters.Add("@dados", SqlDbType.VarBinary, -1).Value = dados;
+            conexao.Open();
+            comando.ExecuteNonQuery();
+        }
+
+        public DataTable GetData()
+        {
+            var dt = new DataTable();
+
+            using var conexao = new SqlConnection(this.StrConexao);
+            using var comando = new SqlDataAdapter("SELECT * FROM TBL", conexao);
+
+            conexao.Open();
+            dt.Load(comando.SelectCommand.ExecuteReader());
+
+            return dt;
+        }
+    }
+}
---
+++ NetCoreAdoNet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Text;
+
+namespace NetCoreAdoNet
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var strConexao = "Data Source=EST-BT13\\SQL2017;Initial Catalog=BDEscola;Integrated Security=SSPI;";
+            var dirFiles = "C:/TESTE";
+
+            var exec = new BD(strConexao);
+            foreach (var file in new System.IO.DirectoryInfo(dirFiles).GetFiles())
+            {
+                var bytes = System.IO.File.ReadAllBytes(file.FullName);
+                exec.RunCommand(bytes);
+            }
+
+            foreach (DataRow dr in exec.GetData().Rows)
+                Console.WriteLine(Encoding.Default.GetString((byte[])dr["dados"]));
+        }
+    }
+}
